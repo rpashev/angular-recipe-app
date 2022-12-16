228d85779e14ea0655d19aeaa267cdace3323545
rpashev <rossen1991@gmail.com>
2022-12-16 23:15:18 +0200

    test deploy

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,9 @@
+// export const environment = {
+//   production: false,
+//   baseURL: 'http://localhost:5000/api/v1/',
+// };
+
 export const environment = {
-  production: false,
-  baseURL: 'http://localhost:5000/api/v1/',
+  production: true,
+  baseURL: 'https://recipe-app-rest.onrender.com/api/v1/',
 };
